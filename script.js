368e35375a615bfe23fe987f2940834f2bacd1f8
Vasilii <vasilii.sporter@gmail.com>
2022-06-13 17:56:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// const userName = "John";
+// let userNumber = 25;
+// userNumber = 24;
+
+// console.log(userName);
+// console.log(userNumber);
+
+// const storeName = 'My awesome store';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'John', 'Sam'],
+//     products: {
+//         'soap': 20,
+//         'water': 5
+//     },
+//     open: true
+// }
+
+//общение с пользователем
+// console.log(storeDescription);
+// console.log(storeDescription.budget);
+// console.log(storeDescription.products.soap);
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt('are you have 18?','18');
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('как вас зовут', '');
+answers[1] = prompt('какая вамилия', '');
+answers[2] = prompt('сколько лет', '');
+
+document.write(answers);
